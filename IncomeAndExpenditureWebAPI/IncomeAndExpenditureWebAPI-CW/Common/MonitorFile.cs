444b65bcad8fb1372b48610db8a30b06922a0bdc
LikeHeart <li30123461@163.com>
2019-06-18 17:15:58 +0800

    新增

---
+++ IncomeAndExpenditureWebAPI/IncomeAndExpenditureWebAPI-CW/Common/MonitorFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeAndExpenditureWebAPI_CW.Common
+{
+    /// <summary>
+    /// 文件监控
+    /// </summary>
+    public class MonitorFile
+    {
+        /// <summary>
+        /// 监控配置文件  
+        /// 配置文件修改后，重新读取配置
+        /// </summary>
+        /// <param name="path">配置文件所在路径</param>
+        /// <param name="settings">配置文件节点</param>
+        public static void MonitorConfig(string path, string[] settings)
+        {
+            FileSystemWatcher fsWatcher = new FileSystemWatcher(path, "*.config");
+            fsWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fsWatcher.Changed += (s, e) =>
+            {
+                try
+                {
+                    settings.ToList().ForEach((setting) =>
+                    {
+                        ConfigurationManager.RefreshSection(setting);
+                    });
+                }
+                catch { }
+            };
+            fsWatcher.EnableRaisingEvents = true;
+        }
+
+    }
+}
---
+++ IncomeAndExpenditureWebAPI/IncomeAndExpenditureWebAPI-CW/Program.cs
@@ -0,0 +1,36 @@
+﻿using IncomeAndExpenditureWebAPI_CW.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace IncomeAndExpenditureWebAPI_CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //监控配置文件中appSettings节点变更后，从磁盘重新读取
+            MonitorFile.MonitorConfig(AppDomain.CurrentDomain.BaseDirectory, new[] { "appSettings" });
+
+            //HostFactory.Run(c =>
+            //{
+            //    c.RunAsLocalSystem();
+            //    c.SetServiceName("UpdateSysTime");
+            //    c.SetDisplayName("更新系统时间");
+            //    c.SetDescription("获取网络时间,更新系统时间");
+
+            //    c.Service<CheckTimeService>(s =>
+            //    {
+            //        s.ConstructUsing(b => new CheckTimeService());
+            //        s.WhenStarted(o => o.Start());
+            //        s.WhenStopped(o => o.Stop());
+            //    });
+            //});
+
+            Console.ReadKey();
+        }
+    }
+}
